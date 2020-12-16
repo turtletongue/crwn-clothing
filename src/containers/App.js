813850d6108254bf9af17c3e4fd35ab810dcb844
platon <platon.s14@list.ru>
2020-12-16 16:59:02 +0500

    Добавление header-а и его стилизация.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from '../components/Header/Header';
 import Homepage from './Homepage/Homepage';
 import HatsPage from './HatsPage/HatsPage';
 import ShopPage from './ShopPage/ShopPage';
@@ -7,6 +8,7 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/hats" exact component={HatsPage} />
