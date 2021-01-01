b030ee3704647a2632ced4ba6f2f013cd6aec479
platon <platon.s14@list.ru>
2021-01-01 16:02:34 +0500

    little fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static.join(__dirname, 'client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.use('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
