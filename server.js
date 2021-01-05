7d39295e3ee7f928ded014f3946ca608bc80370f
platon <platon.s14@list.ru>
2021-01-05 22:32:21 +0500

    Fixing service-worker

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ if (process.env.NODE_ENV === 'production') {
     app.use('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
+
+    app.get("/service-worker.js", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client/build", "service-worker.js"));
+    });
 }
+
 
 app.post('/payment', (req, res) => {
     const body = {
